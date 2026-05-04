36398d2b17d0b46fa4b0fdfdb341852ec1f0dc7d
Cristina <cristinadinca.cd@gmail.com>
2026-05-04 09:43:03 +0200

    Revert "Add files."
    This reverts commit e79f5bdf5832baab39c544a0390a6c6fa766fcb4.

--- src/utils/truncateText.js
+++
@@ -1,5 +0,0 @@
-// utils/truncateText.js
-export const truncateText = (text, maxLength = 300) => {
-	if (!text) return "";
-	return text.length > maxLength ? text.slice(0, maxLength) + "…" : text;
-};
